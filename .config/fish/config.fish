96e5104f9d05008ccfcf02822269f37af8a7982c
christoff-linde <christoff.linde@gmail.com>
2023-10-12 09:11:45 +0200

    chore: update fish config with tabtab

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,6 @@ end
 pyenv init - | source
 
 starship init fish | source
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
