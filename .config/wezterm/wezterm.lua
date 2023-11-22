878bc199be4bf35039d414836ad8de50a364e39a
christoff-linde <christoff.linde@gmail.com>
2023-11-22 12:02:11 +0200

    style(wezterm): add background blur & opacity

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -9,6 +9,10 @@ function scheme_for_appearance(appearance)
 end
 
 return {
+	window_decorations = "RESIZE",
+	window_background_opacity = 0.8,
+	macos_window_background_blur = 24,
+	text_background_opacity = 0.2,
 	-- ...your existing config
 	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
 }
