aa9ceb835cfb574e1ba1ec9b36bc4582a0d9f50a
christoff-linde <christoff.linde@gmail.com>
2023-11-23 07:13:11 +0200

    style(wezterm): update custom font to Iosevka NF

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -13,11 +13,14 @@ return {
 	window_background_opacity = 0.7,
 	macos_window_background_blur = 64,
 	text_background_opacity = 0.4,
-	font = wezterm.font({
-		family = "Monaspace Argon",
-		weight = 500,
-		harfbuzz_features = { "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08", "calt", "dlig" },
+	font = wezterm.font_with_fallback({
+		"Iosevka NF",
+		"JetBrainsMono NF",
+		"FiraCode Nerd Font",
+		"Hack Nerd Font",
+		"MesloLGS NF",
 	}),
+	hafbuzz_features = { "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08", "calt", "dlig" },
 	-- ...your existing config
 	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
 }
