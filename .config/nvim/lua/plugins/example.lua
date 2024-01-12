ec1e5e57ab6fc281ac40d7f84d5393f6041b3212
christoff-linde <christoff.linde@gmail.com>
2024-01-12 12:27:03 +0200

    chore(nvim): reset config to default

--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -1,6 +1,6 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
--- if true then return {} end
+if true then return {} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
@@ -10,15 +10,15 @@
 -- * override the configuration of LazyVim plugins
 return {
   -- add gruvbox
-  -- { "ellisonleao/gruvbox.nvim" },
+  { "ellisonleao/gruvbox.nvim" },
 
   -- Configure LazyVim to load gruvbox
-  -- {
-  --  "LazyVim/LazyVim",
-  --  opts = {
-  --    colorscheme = "gruvbox",
-  --  },
-  -- },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
 
   -- change trouble config
   {
